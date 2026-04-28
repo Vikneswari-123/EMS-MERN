8d95c82226708576366f7efa2cf7ff7689dc55a3
Vikneswari-123 <vikneswarimukunthan@gmail.com>
2026-04-29 00:11:05 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -24,6 +24,8 @@ app.use(cors({
     /\.vercel\.app$/,                    // ← allow all vercel subdomains
   ],
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.json());
 app.use(multer().none());
